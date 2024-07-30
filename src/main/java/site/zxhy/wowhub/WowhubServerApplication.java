13d11bcf5c16ff71a65f5e760d2e77b5f1867db9
amyyxypussy <707978562@qq.com>
2024-07-30 20:20:38 +0800

    init app

---
+++ src/main/java/site/zxhy/wowhub/WowhubServerApplication.java
@@ -0,0 +1,13 @@
+package site.zxhy.wowhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowhubServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowhubServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/zxhy/wowhub/WowhubServerApplicationTests.java
@@ -0,0 +1,13 @@
+package site.zxhy.wowhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowhubServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
