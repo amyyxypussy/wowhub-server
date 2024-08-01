453ca7b86b09a25f19943be48ae7147c5bcb3035
amyyxypussy <707978562@qq.com>
2024-08-01 11:46:45 +0800

    add res

---
+++ src/main/java/site/zxhy/wowhub/config/annotation/AnonymousAccess.java
@@ -0,0 +1,10 @@
+package site.zxhy.wowhub.config.annotation;
+
+import java.lang.annotation.*;
+
+@Inherited
+@Documented
+@Target({ElementType.METHOD,ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AnonymousAccess {
+}
